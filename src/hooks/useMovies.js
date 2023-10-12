79d22c488a688075dc4caabedac46f9863b6c1d4
nescpro <salazarnestor1996@gmail.com>
2023-10-12 13:18:46 -0500

    Prevent the same search from being done twice in a row

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,10 +1,13 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { getMovies } from "../services/movies";
 
 export const useMovies = () => {
   const [movies, setMovies] = useState(null);
+  const search = useRef(null);
 
   const refreshMovies = async (query) => {
+    if (search.current == query) return;
+    search.current = query;
     const movies = await getMovies(query);
     setMovies(movies);
   };
